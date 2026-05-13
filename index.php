a1eaf89c1796a736487b1fe789f19d57cc857c0b
KingRickson <kdelpa@paterostechnologicalcollege.edu.ph>
2026-05-13 10:31:42 +0800

    lab 8

--- index.php
+++ index.php
@@ -5,18 +5,25 @@
 </head>
 <body>
 
-<form method="get">
- Enter a Year: <input type="text" name="year" value="<?php if(isset($_GET['year'])) echo htmlspecialchars($_GET['year']); ?>">
- <input type="submit" value="Check">
+<form method="post">
+ Enter Age: <input type="text" name="age" value="<?php if(isset($_POST['age'])) echo htmlspecialchars($_POST['age']); ?>"><br>
+ Enter Citizenship: <input type="text" name="citizenship" value="<?php if(isset($_POST['citizenship'])) echo htmlspecialchars($_POST['citizenship']); ?>"><br>
+ <input type="submit" value="Check Eligibility">
 </form>
 
 <?php
-if (isset($_GET['year'])) {
-    $year = (int)$_GET['year'];
-    if (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0)) {
-        echo "$year is a Leap Year";
+if (isset($_POST['age']) && isset($_POST['citizenship'])) {
+    $age = (int)$_POST['age'];
+    $citizenship = $_POST['citizenship'];
+
+    if ($age >= 18) {
+        if (strtolower($citizenship) === "filipino") {
+            echo "You are eligible to vote.";
+        } else {
+            echo "You are not eligible to vote. Citizenship requirement not met.";
+        }
     } else {
-        echo "$year is NOT a Leap Year";
+        echo "You are not eligible to vote. Age requirement not met.";
     }
 }
 ?>
