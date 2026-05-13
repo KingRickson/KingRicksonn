63327b41ba419d7c60559187271d09f4ce4125e1
KingRickson <kdelpa@paterostechnologicalcollege.edu.ph>
2026-05-13 10:18:56 +0800

    lab 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+
+$age = 18;
+if ($age >= 18) {
+    echo "Your Eligible to vote.";
+} else {
+    echo "You are a minor.";
+}
+
+?>
+
+</body>
+</html>
